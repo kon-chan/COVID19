bfd76557b73c46d4e7fd7b78899c5c783cca51b0
kon-chan <k.chanworks@gmail.com>
2020-03-31 18:06:05 +0900

    default src

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  /*constructor(props) {
+    super(props);
+    this.state = {count: 0};
+  }*/
+
+  render() {
+    return (
+      <p>test</p>
+    );
+  }
+}
+
+export default App;
